da355440920612386943c3c0098306cfbc87802a
aram1290 <aram1290@gmail.com>
2023-03-30 13:49:38 +0900

    Unity_Ver (0.19)

--- Assets/scripts/SceneManager.cs
+++ Assets/scripts/SceneManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class sceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnClickStart()
     {
-        
+        SceneManager.LoadScene(1);
+        Debug.Log("Start");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickOption()
     {
-        
+        Debug.Log("Option");
+    }
+
+    public void OnClickExit()
+    {
+        Debug.Log("Exit");
     }
 }
