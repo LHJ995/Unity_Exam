810fd2e015f5d2724a71283196efe769bb5fe9ff
aram1290 <aram1290@gmail.com>
2023-04-03 09:25:58 +0900

    Unity_Ver (0.2)

--- Assets/scripts/SceneController.cs
+++ Assets/scripts/SceneController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class scenemanager : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
     public void OnClickStart()
     {
